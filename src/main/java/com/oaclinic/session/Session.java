c3ab8aadd0e3bbce7e49f5ac19f08798907f968a
AvikManna <avik.manna95@gmail.com>
2017-06-18 12:29:16 +0530

    feat: sessionRepository added

--- src/main/java/com/oaclinic/session/Session.java
+++ src/main/java/com/oaclinic/session/Session.java
@@ -1,15 +1,31 @@
 package com.oaclinic.session;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
+@Entity
 public class Session {
-
+	
+	@Id
+	private int id;
 	private String sessionId;
 	private String userName;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getSessionId() {
 		return sessionId;
 	}
