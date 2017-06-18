0660c280bbe17f3b16cf50a05bc628f31e95adfc
subhadip <pramaniksubhadip@gmail.com>
2017-06-19 01:15:57 +0530

    feat: session check implemented

--- src/main/java/com/oaclinic/session/SessionRepository.java
+++ src/main/java/com/oaclinic/session/SessionRepository.java
@@ -3,5 +3,11 @@ package com.oaclinic.session;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SessionRepository extends CrudRepository<Session, Integer>{
+
+	Session findById(String sessionId);
+
+	Session findBySessionId(String sessionId);
+
+	Session findByUserName(String userName);
 	
 }
--- src/main/java/com/oaclinic/test/TestController.java
+++ src/main/java/com/oaclinic/test/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,28 +20,28 @@ public class TestController {
 	private TestService testService;
 
 	@RequestMapping(value = "/tests", method = RequestMethod.GET)
-	public List<Test> getAllTests(@RequestHeader("session-id") String sessionId) {
+	public List<Test> getAllTests() {		
 		return testService.getAllTests();
 	}
 	
 	@RequestMapping(value = "/test/name/{testName}", method = RequestMethod.GET)
-	public Test getTestByName(@PathVariable String testName, @RequestHeader("session-id") String sessionId) {
+	public Test getTestByName(@PathVariable String testName) {
 		return testService.getTestByName(testName);
 	}
 	
 	@RequestMapping(value = "/test/id/{id}", method = RequestMethod.GET)
-	public Test getTestById(@PathVariable String id, @RequestHeader("session-id") String sessionId) {
+	public Test getTestById(@PathVariable String id) {
 		return testService.getTestById(id);
 	}
 	
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
-	public HttpStatus addTest(@RequestBody Test test, @RequestHeader("session-id") String sessionId) {
+	public HttpStatus addTest(@RequestBody Test test) {
 		testService.addTest(test);
 		return HttpStatus.CREATED;
 	}
 	
 	@RequestMapping(value = "/test/id/{id}", method = RequestMethod.POST)
-	public HttpStatus updateTest(@PathVariable int id, @RequestBody Test test, @RequestHeader("session-id") String sessionId) {
+	public HttpStatus updateTest(@PathVariable int id, @RequestBody Test test) {
 		test.setId(id);
 		try {
 			testService.updateTest(test);
