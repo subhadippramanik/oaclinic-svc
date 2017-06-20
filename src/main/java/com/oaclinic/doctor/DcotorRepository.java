e042ed49994e2b529b0bb3966bd398c664745bef
Subhadip <subhadip.pramanik@siemens.com>
2017-06-20 11:15:54 +0530

    Revert "feat: doctorController, doctorService, doctorRepository added."
    This reverts commit cf184e11e8cbd0fa43ce53d06c320e754d2d8a67.

--- src/main/java/com/oaclinic/doctor/DcotorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oaclinic.doctor;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DcotorRepository extends CrudRepository<Doctor, Integer> {
-
-	public Doctor findById(int id);
-}
